77448eac68c350521dad35a41661cf9349531c90
dark-glich <mohdbilal9808464636@gmail.com>
2021-04-02 19:19:08 +0530

    creating equals to function

--- Calculator/main.js
+++ Calculator/main.js
@@ -32,12 +32,116 @@ function decimal() {
         document.getElementById('field').value = field + '.'
     }
 }
-function string_detect() {
-    document.getElementById('field').value = ''
-}
+
 function ClearOne() {
-    field = document.getElementById('field')
-    let last_character = field.charAt(field.length - 1)
-    field = field.replace(last_character, '')
-    document.getElementById('field') = field
+    field = document.getElementById('field').value
+    let striped_field = field.slice(0, -1)
+    document.getElementById('field').value = striped_field
+}
+
+function subtraction(field){
+    let total = 0 
+    let splited_field = field.split('−')
+    for (let i = 0; i < splited_field.length; i++){
+        if(splited_field[i].includes('×') && i == 0){
+            total += multiplication(splited_field[i])
+            
+        }else if(splited_field[i].includes('×') && i != 0){
+            total -= multiplication(splited_field[i])
+            
+        }else if(splited_field[i].includes('÷') && i == 0){
+            total += division(splited_field[i])
+            
+        }else if(splited_field[i].includes('÷') && i != 0){
+            total -= division(splited_field[i])
+            
+        }else if(!(splited_field[i].includes('÷')) && i == 0){
+            total = splited_field[i]
+
+        }else{
+            total -= Number(splited_field[i])
+        }
+    }
+    return total
+}
+function multiplication(field) {
+    let total = 1 
+    let splited_field = field.split('×')
+    for (let i = 0; i < splited_field.length; i++){
+        total *= Number(splited_field[i])
+    }
+    return total
+}
+function division(field) {
+    let total = 1 
+    let splited_field = field.split('÷')
+    for (let i = 0; i < splited_field.length; i++){
+        if (i == 0){
+            total = splited_field[i]
+        }else{
+            total /= Number(splited_field[i])
+        }
+    }
+    return total.toFixed(2);
+}
+
+function PlusSignClicked(field, sum) {
+    let splited_field = field.split('+')
+    for (let i = 0; i < splited_field.length; i++) {
+
+        if(splited_field[i].includes('−')){
+            console.log(sum);
+            sum += subtraction(splited_field[i])
+
+        }else if(splited_field[i].includes('×')){
+            sum += multiplication(splited_field[i])
+
+        }else if(splited_field[i].includes('÷')){
+            sum += division(splited_field[i])
+
+        }else{
+            sum += Number(splited_field[i])
+        }       
+    }
+    return sum
+}
+
+function answer() {
+    field = document.getElementById('field').value
+    let sum = 0
+    if(field.indexOf('+') > -1){
+        sum = PlusSignClicked(field, sum)
+    }/*
+    if(field.indexOf('×') > -1){
+        let splited_field = field.split('×')
+        for (let i = 0; i < splited_field.length; i++){
+            if (i == 0){
+                sum = splited_field[i]
+            }else{
+                sum *= splited_field[i]
+            }
+        }
+    }
+    if(field.indexOf('−') > -1){
+        let splited_field = field.split('−')
+        for (let i = 0; i < splited_field.length; i++){
+            if (i == 0){
+                sum = splited_field[i]
+            }else{
+                sum -= splited_field[i]
+            }
+        }
+    }
+    if(field.indexOf('÷') > -1){
+        let splited_field = field.split('÷')
+        for (let i = 0; i < splited_field.length; i++){
+            if (i == 0){
+                sum = Number(splited_field[i])
+            }else{
+                sum /= splited_field[i]
+            }
+        }
+    }
+    */
+    document.getElementById('field').value = sum
 }
